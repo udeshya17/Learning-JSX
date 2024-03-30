fe8caff759c29a26ec89dce76a645dc9698ea556
Udeshya Sharma <114618734+udeshya17@users.noreply.github.com>
2024-03-30 19:39:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1>My favorite Sport</h1>
+    <ul>
+      <li>Badminton</li>
+      <li>Swiming</li>
+      <li>Cricket</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
